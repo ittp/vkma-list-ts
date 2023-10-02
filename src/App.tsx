80e55b8a90189b7703fb3f8cd0302d3c84faddaa
CodeSandbox <support@codesandbox.io>
2023-10-02 08:28:09 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import {
+  Icon12ChevronOutline,
+  Icon16CommentOutline,
+  Icon16Ghost,
+  Icon16ListPlayOutline,
+  Icon28DevicesOutline,
+} from "@vkontakte/icons";
+import {
   Group,
   Panel,
   PanelHeader,
   SimpleCell,
   View,
   Button,
+  CellButton,
+  Header,
+  ButtonGroup,
+  Cell,
+  RichCell,
 } from "@vkontakte/vkui";
 
 import { Axios, AxiosHeaders, AxiosProxyConfig } from "axios";
@@ -24,9 +36,34 @@ export default function App(config: any) {
     <View activePanel="profile">
       <Panel id="profile">
         <PanelHeader>List</PanelHeader>
+
+        <ButtonGroup>
+          <CellButton mode="primary">B</CellButton>
+        </ButtonGroup>
+
         <Group>
-          <SimpleCell before={"1"}>проспект Ветеранов, 36 корпус 2</SimpleCell>
-          <SimpleCell>world</SimpleCell>
+          <Cell
+            title="title"
+            extraSubtitle="footer"
+            subhead={"subhead"}
+            before={<Button shape="RichCell">B</Button>}
+            after={"after"}
+            target="cell"
+          >
+            <Header>Cell</Header>
+          </Cell>
+          <SimpleCell
+            after={<Icon16CommentOutline />}
+            before={<Icon28DevicesOutline />}
+            expandable
+            title="title"
+            extraSubtitle={"проспект Ветеранов, 36 корпус 2"}
+          >
+            <Group>
+              <Header subtitle={"KN-1111"}>Runner</Header>
+            </Group>
+          </SimpleCell>
+          <SimpleCell>проспект Ветеранов, 166</SimpleCell>
         </Group>
       </Panel>
     </View>
